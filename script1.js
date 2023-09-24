18fb9ed8caeb693d8e23cbaf13da16d2fe01b9b0
Isaac <govegeku00@gmail.com>
2023-09-25 00:33:01 +0300

    Updated Project

--- script1.js
+++ script1.js
@@ -1,31 +1,35 @@
-function calculateGrade() {
+//Challenge 0ne: Student Grade Generator
+function generateGrade() {
     // Prompt the user to enter the input(student's marks)
     const userInput = (prompt("Enter student's marks (between 0 and 100):"));
     const marks = parseFloat(userInput);
   
-    // Check if the input is valid (between 0 and 100)
+    // Check for the validity of the input
     if (isNaN(marks) || marks < 0 || marks > 100) {
       return "Input is invalid. Marks should be between 0 and 100.";
     }
   
-    // Determine the grade based on the grading scale
-    let grade;
-    if (marks > 79) {
-      grade = "A";
-    } else if (marks >= 60) {
-      grade = "B";
-    } else if (marks >= 50) {
-      grade = "C";
-    } else if (marks >= 40) {
-      grade = "D";
-    } else {
-      grade = "E";
-    }
-  
+    // Determine the grade from the userInput
+    switch (true) {
+        case marks > 79:
+          grade = "A";
+          break;
+        case marks >= 60:
+          grade = "B";
+          break;
+        case marks >= 50:
+          grade = "C";
+          break;
+        case marks >= 40:
+          grade = "D";
+          break;
+        default:
+          grade = "E";
+      }
     return `Student's Grade: ${grade}`;
   }
   
-  // Call the function and display the result
-  const studentGrade= calculateGrade();
+  // Display the resultant grade 
+  const studentGrade= generateGrade();
   console.log(studentGrade);
   
--- script3.js
+++ script3.js
@@ -1,25 +1,28 @@
+
 //Net Salary Calculator(Challenge 3)
 // Function to calculate Payee (Tax)
-function calculatePayee(grossIncome) {
-    // Calculate tax based on your country's tax rules
-    // For example, you can use tax brackets and rates
-    // This is a simplified example:
-    if (grossIncome <= 24000) {
-      return 0;
-    } else if (grossIncome <= 40000) {
-      return grossIncome * 0.1;
-    } else {
-      return grossIncome * 0.2;
+function calculatePayee(grossSalary) {
+
+    if (grossSalary <= 24000) {
+      return grossSalary * 0.1;
+    } else if (grossSalary <= 32333) {
+      return grossSalary * 0.25;
+    } else if (grossSalary<= 500000) {
+        return grossSalary *0.3;        
+    } else if(grossSalary<=800000){
+        return grossSalary * 0.325;
+    }else{
+        return grossSalary * 0.35;
     }
-  }
+      
+    }
+  
   
   // Function to calculate NHIF Deductions
-  function calculateNHIF(grossIncome) {
-    // Calculate NHIF deductions based on your country's rules
-    // This is a simplified example:
-    if (grossIncome <= 10000) {
+  function calculateNHIF(grossSalary) {
+    if (grossSalary <= 5999) {
       return 150;
-    } else if (grossIncome <= 20000) {
+    } else if (grossSalary <= 20000) {
       return 300;
     } else {
       return 500;
@@ -28,9 +31,8 @@ function calculatePayee(grossIncome) {
   
   // Function to calculate NSSF Deductions
   function calculateNSSF(grossSalary) {
-    // Calculate NSSF deductions based on your country's rules
-    // This is a simplified example:
-    return grossIncome * 0.05;
+    return grossSalary *0.2;
+
   }
   
   // Function to calculate Net Salary
@@ -58,10 +60,9 @@ function calculatePayee(grossIncome) {
       netSalary,
     };
   }
-  
-  // Example usage:
-  const basicSalary = 30000;
-  const benefits = 5000;
+  //Let's assume Basic Salary and Benefits
+  const basicSalary = 200000;
+  const benefits = 50000;
   
   const salaryDetails = calculateNetSalary(basicSalary, benefits);
   console.log("Gross Salary:", salaryDetails.grossSalary);
@@ -70,3 +71,6 @@ function calculatePayee(grossIncome) {
   console.log("NSSF Deductions:", salaryDetails.nssfDeductions);
   console.log("Net Salary:", salaryDetails.netSalary);
   
+  
+
+
