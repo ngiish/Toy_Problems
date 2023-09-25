318254868014a4ad47627c2e8facd92ba7f8f1d9
Isaac <govegeku00@gmail.com>
2023-09-25 14:54:17 +0300

    Completed Challenges

---
+++ README.md
@@ -0,0 +1,41 @@
+#  TOY PROBLEMS
+*****
+### Author : Isaac Mutiga September 23rd 2023
+****
+## Project Description
+The project contains some of the solutions to week one Toy Problems which were given on Canvas.
+
+
+
+********
+## SetUp Instruction
+### Requirements
+* Text editor eg [Visual Studio Code](https://code.visualstudio.com/download)
+
+
+### Getting Files
+1. Create a repository on your GitHub account. 
+
+NB: One folder, with 3-4 files for each solution. Please note that a single file should only hold one solution.
+
+2. Use JavaScript to wire down the solution.Make appropriate changes in file(s)
+- Run the server to see the changes
+- Add the changes and commit them (git commit -am "Updates")
+
+3. Push the solution to the above repository once done.
+
+4. Submit the repository link for grading.
+
+5. Ensure your repository has a well written README.
+
+
+
+## Technologies Used
+1. JavaScript
+*****
+## Contact Information
+* Email : mutigaim@gmail.com
+*****
+## [License](LICENSE)
+MIT License
+Copyright (c) 2023 Isaac Mutiga
--- studentgradegeneratorchallenge.js
+++ studentgradegeneratorchallenge.js
@@ -1,14 +1,15 @@
 //Challenge 0ne: Student Grade Generator
 function generateGrade() {
     // Prompt the user to enter the input(student's marks)
-    const userInput = (prompt("Enter student's marks (between 0 and 100):"));
+    const userInput = (prompt("Enter student's marks,between 0 and 100:", "Enter a number value"));
+    //use parseFloat to accept the string input and convert it to a floating-point number 
     const marks = parseFloat(userInput);
   
-    // Check for the validity of the input
+    // Check for the validity of the input, brings an error message when user enters a non-number or a number>100, or a number<0
     if (isNaN(marks) || marks < 0 || marks > 100) {
-      return "Input is invalid. Marks should be between 0 and 100.";
+      return "Input is invalid. Marks should be between 0 and 100. Check if you entered a non-number";
     }
-  
+  let grade = isNaN;
     // Determine the grade from the userInput
     switch (true) {
         case marks > 79:
@@ -29,7 +30,7 @@ function generateGrade() {
     return `Student's Grade: ${grade}`;
   }
   
-  // Display the resultant grade 
+  // Display the resultant grade as per the inputted marks
   const studentGrade= generateGrade();
   console.log(studentGrade);
   
